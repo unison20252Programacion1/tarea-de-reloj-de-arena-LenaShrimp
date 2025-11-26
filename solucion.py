cdae6a934a932447ff81a1bd7312e8f00a386681
lena <122074065+LenaShrimp@users.noreply.github.com>
2025-11-25 19:24:42 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -2,8 +2,9 @@ def reloj_arena(m: int, s: str) -> str:
     if m <= 0:
         print("Error: La altura debe ser un entero positivo")
         return ""
+    char = s[0]
     for i in range(m):
-        print(" " * i + s * (2 * (m - i) - 1))
+        print(" " * i + char * (2 * (m - i) - 1))
     for i in range(1, m):
-        print(" " * (m - 1 - i) + s * (2 * i + 1))
+        print(" " * (m - 1 - i) + char * (2 * i + 1))
     return ""
